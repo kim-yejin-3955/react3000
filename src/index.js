24302020ff3829b2200e3a8d3b28a7a2ff118e53
kim-yejin-3955 <80299427+kim-yejin-3955@users.noreply.github.com>
2021-05-25 12:44:06 +0900

    2021.05.25

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const name = "webstoryboy";
-// const element = <h1>hello,{name}</h1>;
 
-// ................1
-
-// function formatName(user){
-//     return user.firstName + user.lastName;
-// }
-
-// const user = {
-//     firstName: "kim",
-//     lastName: "yejin",
-// };
-
-// const element = <h1>hello,{formatName(user)}</h1>;
-// ................2
-
-// const element = <h1 className="hello">hello, kim</h1>;
-
-// ................3
-
-// const element = React.createElement(
-//     "h1",
-//     {className}
-// )
-// ................4
-
-// function clock(){
-//     const element = (
-//         <div>
-//             <h1>hello, yejin</h1>
-//             <h2>It is{new Date().toLocaleDateString()}.</h2>
-//         </div>
-//     );
-//     ReactDOM.render(element, document.getElementById('root') );    
-// }
-// setInterval(clock,1000);
-// ................5
 function Welcome(props){
     return <h1>hello, {props.name}</h1>;
 }
